e0b1902bc1d0cf7fd397e202808ddc094bc33ad3
HengJay <hengjay.wang@itri.org.tw>
2022-01-17 16:23:17 +0800

    Custom Alert.

---
+++ GHFollowers/Utilities/Extension/UIVoewControllerExt.swift
@@ -0,0 +1,21 @@
+//
+//  UIVoewControllerExt.swift
+//  GHFollowers
+//
+//  Created by M100-M1MacMini on 2022/1/17.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    func presentGFAlertOnMainThread(title: String, message: String, buttonTitle: String) {
+        DispatchQueue.main.async {
+            let alertVC = GFAlertVC(title: title, message: message, buttonTitle: buttonTitle)
+            alertVC.modalPresentationStyle = .overFullScreen
+            alertVC.modalTransitionStyle = .crossDissolve
+            self.present(alertVC, animated: true)
+        }
+    }
+    
+}
