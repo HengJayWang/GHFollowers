58f104ec352161a6bee85f82a420ee63490dd5fd
HengJay <hengjay.wang@itri.org.tw>
2022-01-18 16:30:42 +0800

    Refactor error handling by ErrorMessage enum.

---
+++ GHFollowers/Utilities/ErrorMessage.swift
@@ -0,0 +1,15 @@
+//
+//  ErrorMessage.swift
+//  GHFollowers
+//
+//  Created by M100-M1MacMini on 2022/1/18.
+//
+
+import Foundation
+
+enum ErrorMessage: String {
+    case invalidUsername    = "This username created an invalid request.\nPlease try again."
+    case unableToComplete   = "Unable to complete your request.\nPlease check your internet connection."
+    case invalidResponse    = "Invalid response from the server. Please try again."
+    case invalidData        = "The data received from the server was invalid. Please try again."
+}
