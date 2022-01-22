750fed6070546f547a631e7e148f3ea1374cec89
HengJay <hengjay.wang@itri.org.tw>
2022-01-22 17:11:58 +0800

    Network call get userInfo data & GFUserInfoHeaderVC.

---
+++ GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by M100-M1MacMini on 2022/1/22.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
