ec76860ea52e278d4ba33eb8b6ef63dcc5a8c19c
HengJay <hengjay.wang@itri.org.tw>
2022-01-25 15:01:35 +0800

    ItemInfoVC: SuperClass & SubClass

---
+++ GHFollowers/Custom Views/ViewConrollers/ItemInfoVCs/GFRepoItemVC.swift
@@ -0,0 +1,23 @@
+//
+//  GFRepoItemVC.swift
+//  GHFollowers
+//
+//  Created by M100-M1MacMini on 2022/1/25.
+//
+
+import UIKit
+
+class GFRepoItemVC: GFItemInfoVC {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureItems()
+    }
+    
+    
+    private func configureItems() {
+        itemInfoViewOne.set(itemInfoType: .repos, withCount: user.publicRepos)
+        itemInfoViewTwo.set(itemInfoType: .gists, withCount: user.publicGists)
+        actionButton.set(backgroundColor: .systemPurple, title: "GutHub Profile")
+    }
+}
