e67e161d8e5baf96648a909f7de74e7fd43fa9bb
HengJay <hengjay.wang@itri.org.tw>
2022-01-25 18:30:21 +0800

    Miss pushing GFFollowerItemVC.

---
+++ GHFollowers/Custom Views/ViewConrollers/ItemInfoVCs/GFFollowerItemVC.swift
@@ -0,0 +1,23 @@
+//
+//  GFFollowerItemVC.swift
+//  GHFollowers
+//
+//  Created by M100-M1MacMini on 2022/1/25.
+//
+
+import UIKit
+
+class GFFollowerItemVC: GFItemInfoVC {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureItems()
+    }
+    
+    
+    private func configureItems() {
+        itemInfoViewOne.set(itemInfoType: .followers, withCount: user.followers)
+        itemInfoViewTwo.set(itemInfoType: .following, withCount: user.following)
+        actionButton.set(backgroundColor: .systemGreen, title: "Get Followers")
+    }
+}
