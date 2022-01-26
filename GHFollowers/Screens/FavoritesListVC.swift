ee04ddf66a171b8365de72ce3a2f710d0c37178d
HengJay <hengjay.wang@itri.org.tw>
2022-01-26 17:38:48 +0800

    PersistenceManager for saving favorite user.

--- GHFollowers/Screens/FavoritesListVC.swift
+++ GHFollowers/Screens/FavoritesListVC.swift
@@ -12,6 +12,17 @@ class FavoritesListVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBlue
+        
+        PersistenceManager.retrieveFavorites { result in
+            switch result {
+            case .success(let favorites):
+                for favorite in favorites {
+                    print(favorite.login)
+                }
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "Get favorites data fail!", message: error.localizedDescription, buttonTitle: "Ok")
+            }
+        }
     }
     
 }
